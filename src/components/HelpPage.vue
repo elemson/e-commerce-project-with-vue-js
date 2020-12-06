2d576a9f71192804c0607c6b2b2a8163b93af681
elemson <toluwani@juststart.dev>
2020-12-06 22:40:54 +0200

    project commit

---
+++ src/components/HelpPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="help-page">Help page contents here</div>
+</template>
+
+<script>
+export default {
+  name: 'HelpPage',
+  components: {
+  }
+}
+</script>
+
+<style scoped></style>
